3cd9dbc9b77a7e76b6d563892cf2185f291642fd
mangoSu <acid7820@gmail.com>
2022-09-19 15:38:50 +0800

    [feat] add api getTenders、getPublicationOfTender at tenders.service.ts

--- src/app/service/tenders.service.ts
+++ src/app/service/tenders.service.ts
@@ -1,4 +1,4 @@
-import { ApiResponseTenders } from './../types/api-tenders';
+import { TTendersApiResponse } from './../types/api-tenders';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -10,6 +10,10 @@ export class TendersService {
   constructor(private httpClient: HttpClient) { }
 
   getTenders( data: { searchKey: string, page: number } ) {
-    return this.httpClient.get<ApiResponseTenders>(`https://pcc.g0v.ronny.tw/api/searchbytitle?query=${data.searchKey}&page=${data.page}`)
+    return this.httpClient.get<TTendersApiResponse>(`https://pcc.g0v.ronny.tw/api/searchbytitle?query=${data.searchKey}&page=${data.page}`)
+  }
+
+  getPublicationOfTender( data: { unitId: any, jobNumber: any } ) {
+    return this.httpClient.get(`https://pcc.g0v.ronny.tw/api/tender?unit_id=${data.unitId}&job_number=${data.jobNumber}`)
   }
 }
