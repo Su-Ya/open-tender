92be915b0758b2727f1fa2ad7a683015b47b50fa
mangoSu <acid7820@gmail.com>
2022-12-09 16:32:29 +0800

    feat: import MatTabsModule at plugins/angular-material.ts

--- src/app/plugins/angular-material.ts
+++ src/app/plugins/angular-material.ts
@@ -1,8 +1,9 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatTabsModule } from '@angular/material/tabs';
 
 export const AngularMaterialModules = [
   BrowserAnimationsModule,
-  MatTableModule
+  MatTableModule,
+  MatTabsModule
 ]
