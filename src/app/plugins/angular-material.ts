cdef25f8e7963f9b3135c87a0bd2a1682477c7f1
mangoSu <acid7820@gmail.com>
2023-01-18 14:08:18 +0800

    feat: import material loading module

--- src/app/plugins/angular-material.ts
+++ src/app/plugins/angular-material.ts
@@ -1,9 +1,11 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 export const AngularMaterialModules = [
   BrowserAnimationsModule,
   MatTableModule,
-  MatTabsModule
+  MatTabsModule,
+  MatProgressSpinnerModule
 ]
