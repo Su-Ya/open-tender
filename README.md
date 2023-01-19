a32319743f99cb1171df4fcba877e0bb873cb400
mangoSu <acid7820@gmail.com>
2023-01-19 13:31:18 +0800

    rename from demo-open-tender to open-tender

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DemoOpenTender
+# OpenTender
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo-open-tender'`, () => {
+  it(`should have as title 'open-tender'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo-open-tender');
+    expect(app.title).toEqual('open-tender');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('demo-open-tender app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('open-tender app is running!');
   });
 });
